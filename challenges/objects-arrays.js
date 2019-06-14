8d3d6f73028d443ed3f0cb0a0c52b63e04b863d3
Bryan Szendel <bryan.szendel@gmail.com>
2019-06-14 10:14:57 -0600

    Request 2: .map() objects-arrays.js Advanced Array Methods complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -127,7 +127,9 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(obj => {
+  return obj['animal_name'].toLowerCase()
+});
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
